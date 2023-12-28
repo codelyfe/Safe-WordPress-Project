9a42b11d3e3a73fbf1e6ede44c6c3214c1207796
codelyfe <codelyfe@gmail.com>
2023-12-28 09:17:26 -0600

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,12 @@
+// Enable SSL
+define('FORCE_SSL_ADMIN', true);
+define('FORCE_SSL_LOGIN', true);
+
+// Disable File Edit
+define('DISALLOW_FILE_EDIT', true);
+
+// Disable PHP file uploads
+define('DISALLOW_FILE_MODS', true);
+
+// Allowed file types for uploads
+define('ALLOW_UNFILTERED_UPLOADS', false);
