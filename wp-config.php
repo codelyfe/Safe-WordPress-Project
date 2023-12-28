cd6c28e74ccb1af2a94b84aab084d0d8b2910382
codelyfe <codelyfe@gmail.com>
2023-12-28 09:50:46 -0600

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -17,3 +17,32 @@ define('WP_DEBUG', false);
 
 // Set memory limit
 define('WP_MEMORY_LIMIT', '256M'); 
+
+// Limit login Attempts
+define('WP_LIMIT_LOGIN_ATTEMPTS', true);
+
+// Disable trackback pingback
+define('WP_ALLOW_COMMENT_TRACKBACK', false);
+
+// Define CRON job schedule
+define('WP_CRON_LOCK_TIMEOUT', 60);
+
+// Set custom temp directory
+define('WP_TEMP_DIR', '/path/to/custom/temp/dir');
+
+// Set Database Repair Mode
+define('WP_ALLOW_REPAIR', true);
+
+// Define file permissions
+define('FS_CHMOD_DIR', (0705 & ~ umask()));
+define('FS_CHMOD_FILE', (0604 & ~ umask()));
+
+// Define content dir and URL
+define('WP_CONTENT_DIR', '/path/to/your/content');
+define('WP_SITEURL', 'https://www.yourdomain.com');
+
+// Define COOKIE domain
+define('COOKIE_DOMAIN', 'www.yourdomain.com');
+
+// Limit heartbeat API
+define('WP_HEARTBEAT_INTERVAL', 60); // Adjust Heartbeat API interval
