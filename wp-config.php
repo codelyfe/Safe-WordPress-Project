5aade792be11620dbcee5748a395cdd6e2042775
codelyfe <codelyfe@gmail.com>
2023-12-28 09:51:41 -0600

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -46,3 +46,11 @@ define('COOKIE_DOMAIN', 'www.yourdomain.com');
 
 // Limit heartbeat API
 define('WP_HEARTBEAT_INTERVAL', 60); // Adjust Heartbeat API interval
+
+// Disable Emoji Support
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_filter('the_content_feed', 'wp_staticize_emoji');
+remove_filter('comment_text_rss', 'wp_staticize_emoji');
+
+
